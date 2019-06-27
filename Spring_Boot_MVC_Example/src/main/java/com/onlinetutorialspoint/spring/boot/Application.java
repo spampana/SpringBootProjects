896bf0418fb4976f6c0757a5cfa4ef757fc5a4d7
satya <satya.iton@gmail.com>
2019-06-27 21:27:48 +0530

    Setting Application’s TimeZone

--- Spring_Boot_MVC_Example/src/main/java/com/onlinetutorialspoint/spring/boot/Application.java
+++ Spring_Boot_MVC_Example/src/main/java/com/onlinetutorialspoint/spring/boot/Application.java
@@ -1,5 +1,9 @@
 package com.onlinetutorialspoint.spring.boot;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,5 +11,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
+    }
+    
+    @PostConstruct
+    public void init(){
+      // Setting Spring Boot SetTimeZone
+      TimeZone.setDefault(TimeZone.getTimeZone("PST"));
     }
 }
